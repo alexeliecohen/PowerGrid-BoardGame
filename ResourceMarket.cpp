b59e42e346e0948082c1e63791690b818dd10f4e
Mike <39208141+MikeG3@users.noreply.github.com>
2019-02-08 12:23:54 -0500

    Add files via upload

--- ResourceMarket.cpp
+++ ResourceMarket.cpp
@@ -21,15 +21,39 @@ int garbage;
 int uranium;
 
 //Constructors
-ResourceMarket::ResourceMarket() {oil=13;	coal=13;	garbage = 13;	uranium = 13;}
+ResourceMarket::ResourceMarket() {oil=18;	coal=24;	garbage = 6;	uranium = 2;}
 
 ResourceMarket::~ResourceMarket() {}
 
 //Methods
-void addOil() {oil++;}
-void addOil(int amt){oil+=amt;}
-void removeOil(){oil--;}
-void removeOil(int amt){oil-=amt;}
+void addOil() {
+	if (oil<24)
+		oil++;
+	else
+		std::cout << "\nThere is no supply left of oil to add to the resource market\n" ;
+}//close add oil1
+void addOil(int amt){
+	if (oil+amt <= 24)
+		oil+=amt;
+	else {
+		std::cout << "\nThere is no supply left of oil to add to the resource market\n" ;
+		oil = 24;
+	}//close else no oil supply left
+}//close add oil2
+void removeOil(){
+	if (oil>0)
+			oil--;
+		else
+			std::cout << "\nThere is no oil left in the resource market\n" ;
+}//close remove oil
+void removeOil(int amt){
+	if (oil-amt >= 0)
+			oil-=amt;
+		else {
+			std::cout << "\nThere is no oil left in the resource market\n" ;
+			oil = 0;
+		}//close else no oil supply left
+}//close remove oil 2
 int getOil(){return oil;}
 void addCoal(){coal++;}
 void addCoal(int amt){coal+=amt;}
