7b7654c8bf0c1dcee15c8637e7531be2e5bbb530
Hubert <hubalarie@gmail.com>
2019-03-03 16:02:38 -0500

    updating driver

--- Main_PowerGrid_Driver.cpp
+++ Main_PowerGrid_Driver.cpp
@@ -20,12 +20,12 @@
 #include "Map/Map.h"
 
 //Include classes in other files
-//#include "Player.h"
+#include "Powerplant/Player.h"
 //#include "Card.h"
 #include "Powerplant/City.h"
 
 //Name Space
-//using namespace Mike;
+using namespace Mike;
 using std::cout;
 using std::cin;
 using std::endl;
@@ -33,42 +33,42 @@ using std::string;
 
 int main() {
 
-//	//Declare variables
-//	int numOfPlayers;
-//	Player player1;
-//
-//
-//	//Welcome Message
-//	cout << "Hello and welcome to POWER GRID V1.0\n\n" ;
-//
-//	//Prompt users for number of players
-//	numOfPlayers = player1.promptForNumOfPlayers();
-//
-//	cout << "The number of players are: " << numOfPlayers << endl << endl ;
-//
-//	//Create players
-//	//player1.promptPlayerForName();
-//
-//
-//	Player players[numOfPlayers];
-//	//Enter player information
-//	for (int i = 0 ; i < numOfPlayers ; i++)
-//		players[i].promptPlayerForName();
-//
-//
-//	//GARBAGE COLLECTION
-//	delete players;
-///*
-//	for (int i = 0 ; i < numOfPlayers ; i++)
-//		delete players[i];
-//		//players[i].~Player();
-//*/
-//
-//
-//	//exit call and pause
-//	string anykey;
-//	cout << "Press any key to continue";
-//	cin >> anykey;
+	//Declare variables
+	int numOfPlayers;
+	Player player1;
+
+
+	//Welcome Message
+	cout << "Hello and welcome to POWER GRID V1.0\n\n" ;
+
+	//Prompt users for number of players
+	numOfPlayers = player1.promptForNumOfPlayers();
+
+	cout << "The number of players are: " << numOfPlayers << endl << endl ;
+
+	//Create players
+	//player1.promptPlayerForName();
+
+
+	Player players[numOfPlayers];
+	//Enter player information
+	for (int i = 0 ; i < numOfPlayers ; i++)
+		players[i].promptPlayerForName();
+
+
+	//GARBAGE COLLECTION
+	//delete players;
+/*
+	for (int i = 0 ; i < numOfPlayers ; i++)
+		delete players[i];
+		//players[i].~Player();
+*/
+
+
+	//exit call and pause
+	string anykey;
+	cout << "Press any key to continue";
+	cin >> anykey;
     Map *g = new Map();
     Vertex v("Montreal", "Canada");
     Vertex u("Toronto", "Canada");
