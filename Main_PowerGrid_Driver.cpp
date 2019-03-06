bedd1bb931770c59760dc66c7f18863fd0062f57
Hubert <hubalarie@gmail.com>
2019-03-06 18:41:43 -0500

    updating driver

--- Main_PowerGrid_Driver.cpp
+++ Main_PowerGrid_Driver.cpp
@@ -33,49 +33,54 @@ using std::string;
 
 int main() {
 
-	//Declare variables
-	int numOfPlayers;
-	Player player1;
+//	//Declare variables
+//	int numOfPlayers;
+//	Player player1;
+//
+//
+//	//Welcome Message
+//	cout << "Hello and welcome to POWER GRID V1.0\n\n" ;
+//
+//	//Prompt users for number of players
+//	numOfPlayers = player1.promptForNumOfPlayers();
+//
+//	cout << "The number of players are: " << numOfPlayers << endl << endl ;
+//
+//	//Create players
+//	//player1.promptPlayerForName();
+//
+//
+//	Player *players = new Player[numOfPlayers];
+//	//Enter player information
+//	for (int i = 0 ; i < numOfPlayers ; i++)
+//		players[i].promptPlayerForName();
+//
+//
+//	//GARBAGE COLLECTION
+//	//delete players;
+///*
+//	for (int i = 0 ; i < numOfPlayers ; i++)
+//		delete players[i];
+//		//players[i].~Player();
+//*/
+//
+//
+//	//exit call and pause
+//	string anykey;
+//	cout << "Press any key to continue";
+//	cin >> anykey;
 
-
-	//Welcome Message
-	cout << "Hello and welcome to POWER GRID V1.0\n\n" ;
-
-	//Prompt users for number of players
-	numOfPlayers = player1.promptForNumOfPlayers();
-
-	cout << "The number of players are: " << numOfPlayers << endl << endl ;
-
-	//Create players
-	//player1.promptPlayerForName();
-
-
-	Player players[numOfPlayers];
-	//Enter player information
-	for (int i = 0 ; i < numOfPlayers ; i++)
-		players[i].promptPlayerForName();
-
-
-	//GARBAGE COLLECTION
-	//delete players;
-/*
-	for (int i = 0 ; i < numOfPlayers ; i++)
-		delete players[i];
-		//players[i].~Player();
-*/
-
-
-	//exit call and pause
-	string anykey;
-	cout << "Press any key to continue";
-	cin >> anykey;
-    Map *g = new Map();
-    Vertex v("Montreal", "Canada");
-    Vertex u("Toronto", "Canada");
-    g->addVertex(u);
-    g->addVertex(v);
-    std::cout << v.getName();
-    g->addEdge(u, v, 10);
+    Map g = Map();
+    Vertex v = Vertex("Montreal", "Canada");
+    Vertex u = Vertex("Toronto", "Canada");
+    Vertex w = Vertex("New York", "USA");
+    g.addVertex(u);
+    g.addVertex(v);
+    g.addVertex(w);
+    g.addEdge(u, v, 10);
+    g.addEdge(v, w, 12);
+    cout << g;
+    cout << g.BFS(v);
 	return 0;
 }// close main loop
 
