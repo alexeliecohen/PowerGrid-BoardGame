91712114a5066833102ca957a97fa72860a1c3c7
MikeG3 <harveymichaelgarner@hotmail.com>
2019-03-31 16:24:35 -0400

    do it

--- Main_PowerGrid_Driver.cpp
+++ Main_PowerGrid_Driver.cpp
@@ -100,10 +100,54 @@ int main() {
 
     /*
      *  RESOURCE BUYING PHASE
+     *  requires a resource market and players --> hard-coded below
      */
 
-    for ( int i = 0 ; i < players.size() ; i++ )
-        players.at(i).buyResources( resourceMarket );
+    std::cout<<"\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+    std::cout<<"\nWELCOME TO POWER GRID PHASE 3\n";
+    std::cout<<"\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+    std::cout<<"\nCREATING OBJECTS FOR PHASE 3 OF ASSIGNMENT 2\n";
+    std::cout<<"\nCREATING 2 PLAYERS, player1 & player2\n";
 
-	return 0;
+    //Create Players
+    std::vector<Player> playersPhase3 = std::vector<Player>();
+    Player p1 = Player( "player1" );
+    Player p2 = Player( "player2" );
+    playersPhase3.push_back( p1 );
+    playersPhase3.push_back( p2 );
+
+    //Create Map
+    MapLoaderB mapLoaderPart3 = MapLoaderB();
+    Map mapPhase3 = mapLoaderPart3.buildMapB(files.at(mapFile - 1));           //ERROR HERE
+    Map *mapPhase3Ptr = &mapPhase3;
+    std::cout<<"\nThe map has been created for phase 3\n";
+
+    //Create Resource Market
+    ResourceMarket resMarket = ResourceMarket();
+    ResourceMarket *resMarketPtr = &rMarket;
+
+    //Give Power Plants To Player 2
+    std::cout<<"\nPowerplants for player2 are being placed into his/her possesion for demoing's Part 3, buying resources\n";
+
+
+    for ( int i = 0 ; i < playersPhase3.size() ; i++ )
+        playersPhase3.at(i).buyResources( resMarketPtr );
+
+    /*
+     *  HOUSE BUILDING PHASE
+     *  requires a map, resource market and players --> hard-coded below
+     */
+
+    //Source of input parameters above
+    //map: m
+    //gamePhase: 1
+    for ( int i = 0 ; i < players.size() ; i++ )
+        players.at(i).buyCities( mapPhase3Ptr, 1 );
+
+    /*
+     *  END OF PART 3 ASSIGNMENT 2
+     */
+
+
+    return 0;
 }// close main loop
